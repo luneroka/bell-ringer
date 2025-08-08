04230be4319cb0bbbc07fa58aa5fa408dc16405c
Luneroka <luneroka@gmail.com>
2025-08-08 13:08:20 +0200

    Initialize Spring Boot project

---
+++ src/main/java/com/bell_ringer/BellRingerApplication.java
@@ -0,0 +1,13 @@
+package com.bell_ringer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellRingerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellRingerApplication.class, args);
+	}
+
+}
