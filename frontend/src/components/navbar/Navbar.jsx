80cb1cd4710e901755215a22e0a865c52d9e3f18
Luneroka <luneroka@gmail.com>
2025-08-18 17:06:10 +0200

    Make the navbar fixed and none shrinkable

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,15 +3,16 @@ import { CiUser, CiSettings } from 'react-icons/ci';
 function Navbar() {
   return (
     <header
-      className='bg-primary position-sticky top-0 rounded-2'
+      className='bg-primary rounded-2'
       style={{
+        position: 'fixed',
+        left: '32px',
+        top: '96px',
         width: '4rem',
         height: 'calc(100vh - 192px)',
-        minHeight: '680px',
-        marginTop: '96px',
-        marginBottom: '96px',
-        marginLeft: '32px',
+        minHeight: '480px',
         overflow: 'auto',
+        zIndex: 1030,
       }}
     >
       <nav
