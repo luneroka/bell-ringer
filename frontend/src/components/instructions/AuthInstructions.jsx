3c4518e6cba715d950a0a19b4502bb289bffb636
Luneroka <luneroka@gmail.com>
2025-08-19 21:56:55 +0200

    Minor styling of instructions components

--- frontend/src/components/instructions/AuthInstructions.jsx
+++ frontend/src/components/instructions/AuthInstructions.jsx
@@ -2,14 +2,19 @@ function AuthInstructions() {
   return (
     <>
       <div className='d-fex flex-column gap-4 bg-light p-4 rounded-3'>
-        <p className='font-family-primary'>
+        <p>
           Enter your settings below to generate a quiz tailored to your goals.
         </p>
-        <p className='font-family-primary'>
-          Duo mode: for each question, the answer can be revealed by the person
-          quizzing you. Solo mode: simply answer the questions to reveal the
-          answer.
-        </p>
+        <div>
+          <p>
+            <strong>Duo mode:</strong> for each question, the answer can be
+            revealed by the person quizzing you.
+          </p>
+          <p>
+            <strong>Solo mode:</strong> simply answer the questions to reveal
+            the answer.
+          </p>
+        </div>
       </div>
     </>
   );
--- frontend/src/components/instructions/IndexInstructions.jsx
+++ frontend/src/components/instructions/IndexInstructions.jsx
@@ -7,14 +7,14 @@ function IndexInstructions() {
         <p className='font-family-primary fw-semibold'>
           Welcome to Bell Ringer !
         </p>
-        <p className='font-family-primary'>
+        <p>
           Bell Ringer is a revision platform designed to make learning web
           development more interactive and enjoyable. <br />
           Whether you’re on your own or with a partner, you can choose a topic,
           enable shuffle mode, and test your knowledge through questions created
           to help you progress.
         </p>
-        <p className='font-family-primary'>
+        <p>
           The idea behind Bell Ringer is simple: to offer a user-friendly tool
           suited for both pair revisions (your partner quizzes you) and solo
           practice.
